cf66f5ca3367a9e9b76a33957b70a52f0e542e7a
zekiahepic <73035340+Zekiah-A@users.noreply.github.com>
2021-01-20 17:33:45 +0000

    Add files via upload

---
+++ Generator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int diameter;
+            Console.WriteLine("Enter the width of circle:");
+            String input = Console.ReadLine();
+            diameter = Int32.Parse(input);
+            Console.WriteLine("\n");
+            diameter /= 2;
+            int x; int y; int a;
+            a = (2 * diameter) + 1;
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < a; j++)
+                {
+                    y=j-diameter;
+                    x=i-diameter;
+
+                    if (x*x + y*y <= (diameter*diameter + 1))
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write(" ");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
